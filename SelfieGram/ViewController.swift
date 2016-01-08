df66b64fbfe4c415e6d4a2d13b4f86c5c7fa6f8f
mchen0901 <mchen0901@gmail.com>
2016-01-07 20:03:29 -0800

    Initial Commit

---
+++ SelfieGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfieGram
+//
+//  Created by Michael Chen on 2016-01-07.
+//  Copyright © 2016 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
